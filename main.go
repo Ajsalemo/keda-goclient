2289f2d9f48b902060d7e185b76ea1e68e03eb56
Anthony Salemo <ajssalemo@gmail.com>
2025-02-17 16:11:23 -0500

    able to query keda resources

--- main.go
+++ main.go
@@ -6,12 +6,48 @@ import (
 	"fmt"
 	"path/filepath"
 
-	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
+
+var (
+	kedaGVRJobs = schema.GroupVersionResource{
+		Group:    "keda.sh",
+		Version:  "v1alpha1",
+		Resource: "scaledjobs",
+	}
+)
+
+var (
+	kedaGVRObject = schema.GroupVersionResource{
+		Group:    "keda.sh",
+		Version:  "v1alpha1",
+		Resource: "scaledobjects",
+	}
+)
+
+func kedaGVR() schema.GroupVersionResource {
+	// Mock function. Testing to see if we can switch between object types / different clients
+	scaledObject := false
+	if scaledObject {
+		return kedaGVRObject
+	} else {
+		return kedaGVRJobs
+	}
+}
+
+func GetScaledObjectByName(dynamicClient *dynamic.DynamicClient, name string) {
+	scaledObjectClient := dynamicClient.Resource(kedaGVR())
+	scaledObject, err := scaledObjectClient.Namespace("default").Get(context.TODO(), name, metav1.GetOptions{})
+	if err != nil {
+		fmt.Printf("Error retrieving scaledjobs: %s", err)
+	} else {
+		fmt.Printf("Got ScaledObject: %v", scaledObject)
+	}
+}
 
 func main() {
 	var kubeconfig *string
@@ -26,17 +62,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+
+	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 
-	client := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-	getDeployments, err := client.List(context.TODO(), metav1.ListOptions{})
-
-	if err != nil {
-		for _, d := range getDeployments.Items {
-			fmt.Println(d.Name)
-		}
-	}
+	GetScaledObjectByName(dynamicClient, "github-runner")
 }
