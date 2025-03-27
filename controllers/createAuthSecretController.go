d336acabcbf6e9bfb67be3522193f92d342221a5
Anthony Salemo <ajssalemo@gmail.com>
2025-03-27 08:52:32 -0400

    updated secret logic handling

--- controllers/createAuthSecretController.go
+++ controllers/createAuthSecretController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	config "github.com/Ajsalemo/keda-goclient/config"
 	"github.com/gofiber/fiber/v2"
@@ -38,6 +39,31 @@ func CreateAuthSecret(c *fiber.Ctx) error {
 		return c.Status(500).JSON(fiber.Map{"error": secretErr.Error()})
 	}
 
-	zap.L().Info("Created secret " + secret.ObjectMeta.Name)
-	return c.SendString("Created secret " + secret.ObjectMeta.Name)
+	ticker := time.NewTicker(500 * time.Millisecond)
+	defer ticker.Stop()
+	start := time.Now()
+	for {
+		select {
+		case <-ticker.C:
+			secret, err := clientset.CoreV1().Secrets("apps").Get(context.TODO(), authenticationSecretStruct.Name, metav1.GetOptions{})
+			if err != nil {
+				zap.L().Error(err.Error())
+				return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+			}
+
+			elapsed := time.Since(start)
+			zap.L().Info("Polling secret creation: " + elapsed.String())
+			// If the elapsed time is greater than 60 seconds, return a 500
+			if elapsed > 60*time.Second {
+				zap.L().Info("Elapsed time: " + elapsed.String())
+				zap.L().Warn("Secret creation took longer than 30 seconds, exiting")
+				return c.Status(500).JSON(fiber.Map{"error": "Creation took longer than 30 seconds"})
+			}
+			zap.L().Info("Polling for secret creation progress..")
+			if secret.GetName() != "" {
+				zap.L().Info("Kind 'Secret' has been created with name " + secret.ObjectMeta.Name)
+				return c.SendString("Created secret " + secret.ObjectMeta.Name)
+			}
+		}
+	}
 }
